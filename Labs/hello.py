46def2e6782c682a8775fb04abc3b23b7e3ab0d9
Bryan Ow <2603026@sit.singaporetech.edu.sg>
2026-09-04 11:34:34 +0800

    Add follower count

--- Labs/hello.py
+++ Labs/hello.py
@@ -7,6 +7,16 @@ username = "cool_coder"
 bio = "Fun coder"
 followers = 100
 
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers -= 30
+print("Day 3:", followers)
+
+
 print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
